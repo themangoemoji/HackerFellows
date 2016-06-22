1c1676e214e68d0655cab0be7adbbbf8b9184c8a
Michael Wright <wrigh517@gmail.com>
2016-06-22 10:54:30 -0400

    Move lessons into modules instead of weeks

---
+++ Angular/angular-hw-2/app.js
@@ -0,0 +1,14 @@
+//Create an angular module with ngroute as dependency for routing views
+//  the module will contain the router and controller
+var app = angular.module('madlibModule', []);
+
+//This will configure routes for the partial views which include
+//  home, data, and display.
+//The config takes a route provider
+app.config(function($routeProvider) {
+
+  ngRoute provides
+  $routeProvider
+
+
+});
